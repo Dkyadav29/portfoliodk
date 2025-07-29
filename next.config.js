206c8e4b3935f58569eaa6976896bbf7f58d69ec
Dkyadav29 <dkyadav2905@gmail.com>
2025-07-29 12:13:32 +0530

    Fix: Use next.config.js for Vercel compatibility

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // optional: include other settings
-  reactStrictMode: true,
-  // experimental: { appDir: true } // if you're using the app directory
-};
-
-module.exports = nextConfig;
